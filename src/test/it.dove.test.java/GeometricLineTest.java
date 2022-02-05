fe3b572319fd3b2a4c7a732b6a1b0eee0dc39ff0
GiaMoscariello <gia.moscariello@gmail.com>
2022-02-05 19:16:31 +0100

    Add and fix test

--- src/test/it.dove.test.java/GeometricLineTest.java
+++ src/test/it.dove.test.java/GeometricLineTest.java
@@ -1,5 +1,5 @@
 
-import model.*;
+import it.dove.model.*;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -24,7 +24,7 @@ public class GeometricLineTest {
     public void implicitLineWithCoefficientsZeroShouldFail() {
         BigDecimal a = BigDecimal.ZERO;
         BigDecimal b = BigDecimal.ZERO;
-        BigDecimal c = BigDecimal.valueOf(1);
+        BigDecimal c = BigDecimal.ONE;
 
         assertThrows(NumberFormatException.class, () -> new ImplicitLine(a, b, c));
     }
@@ -57,28 +57,30 @@ public class GeometricLineTest {
         BigDecimal x = BigDecimal.valueOf(5);
         BigDecimal y = BigDecimal.valueOf(12);
         BigDecimal m = BigDecimal.valueOf(2);
+        PointBigDecimal point = new PointBigDecimal(x, y);
 
-        GeometricLine actualLine = new PointLine(x, y, m);
+        GeometricLine actualLine = new PointLine(point, m);
 
         assertEquals(expected, actualLine.angularCoefficient());
     }
 
     @Test
-    public void cantCalculateAngularCoeff() {
+    public void pointsLineWithNoAngularCoefficientShouldFail() {
         BigDecimal x = BigDecimal.valueOf(5);
         BigDecimal y = BigDecimal.valueOf(12);
+        PointBigDecimal point = new PointBigDecimal(x, y);
 
-        GeometricLine actualLine = new PointLine(x, y, null);
+        GeometricLine line = new PointLine(point, null);
 
-        assertThrows(NumberFormatException.class, actualLine::angularCoefficient);
+        assertThrows(NumberFormatException.class, line::angularCoefficient);
     }
-
 
     @Test
     public void linesAreNotParallel() {
         BigDecimal a = BigDecimal.valueOf(2);
         BigDecimal b = BigDecimal.valueOf(1);
         BigDecimal c = BigDecimal.valueOf(1);
+
 
         BigDecimal m = BigDecimal.valueOf(2);
         BigDecimal q = BigDecimal.valueOf(1);
@@ -90,16 +92,46 @@ public class GeometricLineTest {
     }
 
     @Test
-    public void linesAreParallel() {
-        BigDecimal a = BigDecimal.valueOf(-4);
-        BigDecimal b = BigDecimal.valueOf(2);
-        BigDecimal c = BigDecimal.valueOf(1);
+    public void linesAreParallelImplicits() {
+        BigDecimal a1 = BigDecimal.valueOf(-4);
+        BigDecimal b1 = BigDecimal.valueOf(2);
+        BigDecimal c1 = BigDecimal.valueOf(1);
 
+        BigDecimal a2 = BigDecimal.valueOf(-8);
+        BigDecimal b2 = BigDecimal.valueOf(4);
+        BigDecimal c2 = BigDecimal.valueOf(2);
+
+        GeometricLine line3 = new ImplicitLine(a1, b1, c1);
+        GeometricLine line4 = new ImplicitLine(a2, b2, c2);
+
+        assertTrue(line3.isParallelWith(line4));
+    }
+
+    @Test
+    public void linesAreParallelExplicits() {
+
+        BigDecimal m1 = BigDecimal.valueOf(-2);
+        BigDecimal q1 = BigDecimal.valueOf(12);
+
+        BigDecimal m2 = BigDecimal.valueOf(-2);
+        BigDecimal q2 = BigDecimal.valueOf(4);
+
+        GeometricLine line1 = new ExplicitLine(m1, q1);
+        GeometricLine line2 = new ExplicitLine(m2, q2);
+
+        assertTrue(line1.isParallelWith(line2));
+    }
+
+    @Test
+    public void linesAreParallelPoints() {
+        BigDecimal x = BigDecimal.valueOf(5);
+        BigDecimal y = BigDecimal.valueOf(12);
         BigDecimal m = BigDecimal.valueOf(2);
-        BigDecimal q = BigDecimal.valueOf(1);
 
-        GeometricLine line1 = new ExplicitLine(m, q);
-        GeometricLine line2 = new ImplicitLine(a, b, c);
+        PointBigDecimal point = new PointBigDecimal(x, y);
+
+        GeometricLine line1 = new PointLine(point, m);
+        GeometricLine line2 = new PointLine(point, m);
 
         assertTrue(line1.isParallelWith(line2));
     }
@@ -107,18 +139,17 @@ public class GeometricLineTest {
     @Test
     public void linesArePerpendicular() {
         BigDecimal a1 = BigDecimal.valueOf(4);
-        BigDecimal b1 = BigDecimal.valueOf(3);
-        BigDecimal c1 = BigDecimal.valueOf(0);
+        BigDecimal b1 = BigDecimal.valueOf(8);
+        BigDecimal c1 = BigDecimal.valueOf(3);
 
         BigDecimal a2 = BigDecimal.valueOf(-6);
-        BigDecimal b2 = BigDecimal.valueOf(8);
+        BigDecimal b2 = BigDecimal.valueOf(12);
         BigDecimal c2 = BigDecimal.valueOf(5);
 
-
-        BigDecimal m1 = BigDecimal.valueOf(-5.0);
+        BigDecimal m1 = BigDecimal.valueOf(-1);
         BigDecimal q1 = BigDecimal.valueOf(12);
 
-        BigDecimal m2 = BigDecimal.valueOf(Math.pow(5, -1));
+        BigDecimal m2 = BigDecimal.valueOf(1);
         BigDecimal q2 = BigDecimal.valueOf(4);
 
         GeometricLine line1 = new ImplicitLine(a1, b1, c1);
@@ -129,5 +160,6 @@ public class GeometricLineTest {
 
         assertTrue(line1.isPerpendicularWith(line2));
         assertTrue(line3.isPerpendicularWith(line4));
+        assertTrue(line1.isParallelWith(line3));
     }
 }
